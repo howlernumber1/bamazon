2ba20ba6940400f696b93a3ec61819a8595dff47
markj06 <mjohnson4406@outlook.com>
2018-11-15 22:36:20 -0500

    d

--- config/config.js
+++ config/config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  development: {
-    username: "root",
-    password: process.env.DB_PASS,
-    database: "db",
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql"
-  },
-  test: {
-    username: "root",
-    password: process.env.DB_PASS,
-    database": "db",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  production: {
-use_env_variable: "JAWSDB_URL"
-  }
+    development: {
+        username: "mj",
+        password: process.env.DB_PASS,
+        database: "Bamazon",
+        host: "localhost",
+        port: 3306,
+        dialect: "mysql"
+    },
+    test: {
+        username: "root",
+        password: process.env.DB_PASS,
+        database: "db",
+        host: "127.0.0.1",
+        dialect: "mysql"
+    },
+    production: {
+        use_env_variable: "JAWSDB_URL"
+    }
 }
