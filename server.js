d47477dfc46dade2fd5b98fe99a1ba919aba646a
markj06 <mjohnson4406@outlook.com>
2018-11-15 21:30:44 -0500

    updated env variable

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./models')
 const app = express();
+const env = process.env.NODE_ENV || 'development';
 
 // Defines a PORT for the server to listen for requests
    const PORT = process.env.PORT || 3000;
